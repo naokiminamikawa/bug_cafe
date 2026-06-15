143406e3b757db494fcbb96bb7aedc750ecfdbb6
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-06-15 22:27:41 +0000

    カフェプログラムのバグ修正

--- cafe.rb
+++ cafe.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# require 'debug'
 
 DRINKS = [
   { name: 'コーヒー', price: '300' },
@@ -15,12 +16,13 @@ FOODS = [
 ].freeze
 
 def take_order(menus)
+  # binding.break
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
+  puts "#{menus[order_number - 1][:name]}(#{menus[order_number - 1][:price]}円)ですね。"
   order_number
 end
 
@@ -30,5 +32,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = FOODS[order2 - 1][:price].to_i + DRINKS[order1 - 1][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
