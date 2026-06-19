7509a2f3a8b13f3e3d34edc6fbc1ebc561411a0b
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-06-19 08:22:56 +0000

    デバッグコード削除・index変数名変更・価格の整数定義

--- cafe.rb
+++ cafe.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
-# require 'debug'
 
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
-  # binding.break
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number - 1][:name]}(#{menus[order_number - 1][:price]}円)ですね。"
-  order_number
+
+  index = order_number - 1
+  puts "#{menus[index][:name]}(#{menus[index][:price]}円)ですね。"
+  index
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_index = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_index = take_order(FOODS)
 
-total = FOODS[order2 - 1][:price].to_i + DRINKS[order1 - 1][:price].to_i
+total = FOODS[food_index][:price] + DRINKS[drink_index][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
